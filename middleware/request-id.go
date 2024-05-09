7d70b11b8087838e3680ad5d2e5153bf63445764
Rodrigo Masini <104578949+rodrigomasiniai@users.noreply.github.com>
2024-05-09 13:57:06 -0300

    Update request-id.go

--- middleware/request-id.go
+++ middleware/request-id.go
@@ -3,7 +3,7 @@ package middleware
 import (
 	"context"
 	"github.com/gin-gonic/gin"
-	"github.com/songquanpeng/one-api/common/helper"
+	"github.com/vantech-neonetics/fm-management-app/common/helper"
 )
 
 func RequestId() func(c *gin.Context) {
