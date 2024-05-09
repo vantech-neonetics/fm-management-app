0ad1452da232ed4075a15c58063a93878dfd30bd
Rodrigo Masini <104578949+rodrigomasiniai@users.noreply.github.com>
2024-05-09 13:48:36 -0300

    Update adaptor.go

--- relay/adaptor.go
+++ relay/adaptor.go
@@ -1,38 +1,34 @@
 package relay
 
 import (
-	"github.com/songquanpeng/one-api/relay/adaptor"
-	"github.com/songquanpeng/one-api/relay/adaptor/aiproxy"
-	"github.com/songquanpeng/one-api/relay/adaptor/ali"
-	"github.com/songquanpeng/one-api/relay/adaptor/anthropic"
-	"github.com/songquanpeng/one-api/relay/adaptor/aws"
-	"github.com/songquanpeng/one-api/relay/adaptor/baidu"
-	"github.com/songquanpeng/one-api/relay/adaptor/cloudflare"
-	"github.com/songquanpeng/one-api/relay/adaptor/cohere"
-	"github.com/songquanpeng/one-api/relay/adaptor/coze"
-	"github.com/songquanpeng/one-api/relay/adaptor/deepl"
-	"github.com/songquanpeng/one-api/relay/adaptor/gemini"
-	"github.com/songquanpeng/one-api/relay/adaptor/ollama"
-	"github.com/songquanpeng/one-api/relay/adaptor/openai"
-	"github.com/songquanpeng/one-api/relay/adaptor/palm"
-	"github.com/songquanpeng/one-api/relay/adaptor/tencent"
-	"github.com/songquanpeng/one-api/relay/adaptor/xunfei"
-	"github.com/songquanpeng/one-api/relay/adaptor/zhipu"
-	"github.com/songquanpeng/one-api/relay/apitype"
+	"github.com/vantech-neonetics/fm-management-app/relay/adaptor"
+	"github.com/vantech-neonetics/fm-management-app/relay/adaptor/aiproxy"
+	"github.com/vantech-neonetics/fm-management-app/relay/adaptor/ali"
+	"github.com/vantech-neonetics/fm-management-app/relay/adaptor/anthropic"
+	"github.com/vantech-neonetics/fm-management-app/relay/adaptor/aws"
+	"github.com/vantech-neonetics/fm-management-app/relay/adaptor/baidu"
+	"github.com/vantech-neonetics/fm-management-app/relay/adaptor/cloudflare"
+	"github.com/vantech-neonetics/fm-management-app/relay/adaptor/cohere"
+	"github.com/vantech-neonetics/fm-management-app/relay/adaptor/coze"
+	"github.com/vantech-neonetics/fm-management-app/relay/adaptor/deepl"
+	"github.com/vantech-neonetics/fm-management-app/relay/adaptor/gemini"
+	"github.com/vantech-neonetics/fm-management-app/relay/adaptor/ollama"
+	"github.com/vantech-neonetics/fm-management-app/relay/adaptor/openai"
+	"github.com/vantech-neonetics/fm-management-app/relay/adaptor/palm"
+	"github.com/vantech-neonetics/fm-management-app/relay/adaptor/tencent"
+	"github.com/vantech-neonetics/fm-management-app/relay/adaptor/xunfei"
+	"github.com/vantech-neonetics/fm-management-app/relay/adaptor/zhipu"
+	"github.com/vantech-neonetics/fm-management-app/relay/apitype"
 )
 
 func GetAdaptor(apiType int) adaptor.Adaptor {
 	switch apiType {
 	case apitype.AIProxyLibrary:
 		return &aiproxy.Adaptor{}
-	case apitype.Ali:
-		return &ali.Adaptor{}
 	case apitype.Anthropic:
 		return &anthropic.Adaptor{}
 	case apitype.AwsClaude:
 		return &aws.Adaptor{}
-	case apitype.Baidu:
-		return &baidu.Adaptor{}
 	case apitype.Gemini:
 		return &gemini.Adaptor{}
 	case apitype.OpenAI:
@@ -41,14 +37,8 @@ func GetAdaptor(apiType int) adaptor.Adaptor {
 		return &palm.Adaptor{}
 	case apitype.Tencent:
 		return &tencent.Adaptor{}
-	case apitype.Xunfei:
-		return &xunfei.Adaptor{}
-	case apitype.Zhipu:
-		return &zhipu.Adaptor{}
 	case apitype.Ollama:
 		return &ollama.Adaptor{}
-	case apitype.Coze:
-		return &coze.Adaptor{}
 	case apitype.Cohere:
 		return &cohere.Adaptor{}
 	case apitype.Cloudflare:
