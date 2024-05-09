9f46d668a353d6c533e2663946bedb89b7ab6f0f
Rodrigo Masini <104578949+rodrigomasiniai@users.noreply.github.com>
2024-05-09 13:45:59 -0300

    Update relay.go

--- router/relay.go
+++ router/relay.go
@@ -1,8 +1,8 @@
 package router
 
 import (
-	"github.com/songquanpeng/one-api/controller"
-	"github.com/songquanpeng/one-api/middleware"
+	"github.com/vantech-neonetics/fm-management-app/controller"
+	"github.com/vantech-neonetics/fm-management-app/middleware"
 
 	"github.com/gin-gonic/gin"
 )
