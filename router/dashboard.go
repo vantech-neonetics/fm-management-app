dbd00a423478e92191d0e6e1eedef7943ae56f64
Rodrigo Masini <104578949+rodrigomasiniai@users.noreply.github.com>
2024-05-09 13:44:46 -0300

    Update dashboard.go

--- router/dashboard.go
+++ router/dashboard.go
@@ -3,8 +3,8 @@ package router
 import (
 	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
-	"github.com/songquanpeng/one-api/controller"
-	"github.com/songquanpeng/one-api/middleware"
+	"github.com/vantech-neonetics/fm-management-app/controller"
+	"github.com/vantech-neonetics/fm-management-app/middleware"
 )
 
 func SetDashboardRouter(router *gin.Engine) {
