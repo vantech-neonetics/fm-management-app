0a10ec0c55380098dcc2295380856a28b2a0a2fd
Rodrigo Masini <104578949+rodrigomasiniai@users.noreply.github.com>
2024-05-14 08:59:19 -0300

    Update logger.go

--- common/logger/logger.go
+++ common/logger/logger.go
@@ -11,8 +11,8 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/songquanpeng/one-api/common/config"
-	"github.com/songquanpeng/one-api/common/helper"
+	"github.com/vantech-neonetics/fm-management-app/common/config"
+	"github.com/vantech-neonetics/fm-management-app/common/helper"
 )
 
 const (
