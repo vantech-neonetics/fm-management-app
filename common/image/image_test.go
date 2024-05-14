1a438ce522c7b5e06c9aaadefe7dda4205909657
Rodrigo Masini <104578949+rodrigomasiniai@users.noreply.github.com>
2024-05-14 08:58:38 -0300

    Update image_test.go

--- common/image/image_test.go
+++ common/image/image_test.go
@@ -12,7 +12,7 @@ import (
 	"strings"
 	"testing"
 
-	img "github.com/songquanpeng/one-api/common/image"
+	img "github.com/vantech-neonetics/fm-management-app/common/image"
 
 	"github.com/stretchr/testify/assert"
 	_ "golang.org/x/image/webp"
